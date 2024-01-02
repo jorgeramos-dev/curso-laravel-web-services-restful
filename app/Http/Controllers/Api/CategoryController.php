bedf2c827214dee4c18640b45113a77353ab40cb
jorgeramos-dev <dev.jorgeramos@gmail.com>
2024-01-02 18:41:05 -0300

    create form request category

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Requests\CategoriesFormRequest;
 
 class CategoryController extends Controller
 {
