4de72b2eb851eb106e89ba26298b46a66c0d76db
jorgeramos-dev <dev.jorgeramos@gmail.com>
2024-01-18 19:25:24 -0300

    created products controller

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Products;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    private $product;
+
+    public function __construct(Products $product)
+    {
+        $this->product = $product;
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json($product);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
